761bf4d6f3a5ad1e43ed3cee3210bed5f2fb79d1
Julio Schilders <julio.schilders@gmail.com>
2019-07-13 15:14:45 +0200

    Updates

---
+++ precache-manifest.a94236d245f6b4d1e4f2c62fe5430633.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035e4e5d8bebc100400b47533e3fc427",
+    "url": "/shopia/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/shopia/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ccb91843afb11e69ea40",
+    "url": "/shopia/static/js/main.9f507c06.chunk.js"
+  },
+  {
+    "revision": "8b3a2397c8f9bdffb893",
+    "url": "/shopia/static/js/runtime~main.f7574200.js"
+  }
+]);
